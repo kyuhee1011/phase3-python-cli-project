d60260c73ab3b4d6adc8aceb9b5dda6776a68ae6
Kyuhee Lee <kyuhee.lee.1011@gmail.com>
2023-08-15 23:28:55 -0400

    create two join tables

--- lib/db/models.py
+++ lib/db/models.py
@@ -1,24 +1,65 @@
-from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, Table
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
 
 Base = declarative_base()
 
+player_character=Table (
+    "player_character",
+    Base.metadata,
+    Column ("player_character_id", Integer, primary_key=True),
+    Column ("character_id", ForeignKey("character.id")),
+    Column ("player_id", ForeignKey("player.id"))
+)
+
+room =Table(
+    "room_character_enemy",
+    Base.metadata,
+    Column ("room_id", Integer, primary_key=True),
+    Column ("room name",String()),
+    Column ("room type",String()),
+    Column ("character_id", ForeignKey("character.id")),
+    Column ("enemy_id", ForeignKey("enemy.id"))
+)
+
+class Player(Base):
+    __tablename__='players'
+ 
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
+    number_of_win= Column (Integer())
+    number_of_lose=Column (Integer())
+    
+    characters=relationship("Character", secondary=player_character, backref="the_players")
+
+    def __repr__(self):
+        return f"Players {self.id}: " \
+            + f"{self.name}, " \
+            + f"Winning Score {self.number_of_win}"\
+            + f"Losing Score {self.number_of_lose}"
+    
+   
+
 class Character (Base): 
     __tablename__='characters'
-    id = Column (Integer(), primary_key=True)
+    id = Column(Integer(), primary_key=True)
     name = Column(String())
     class_job= Column(String())
     health_point = Column(Integer())
+    combat_word = Column (String())
     attack_point = Column(Integer())
+
+    players=relationship("Player", secondary=player_character, backref="the_characters")
+    enemies=relationship("Enemy", secondary=room, backref="the_enemies")
 
     def __repr__(self):
         return f"Character {self.id}: " \
             + f"{self.name}, " \
             + f"class {self.class_job}"\
             + f"HP {self.health_point}"\
+             + f"Combat Word {self.combat_word}"\
             + f"AP {self.attack_point}"
-
+    
 class Enemy (Base): 
     __tablename__='enemies'
     id = Column (Integer(), primary_key=True)
@@ -26,7 +67,7 @@ class Enemy (Base):
     type= Column(String())
     health_point = Column(Integer())
     attack_point = Column(Integer())
-    room_id = Column(String(), ForeignKey ("rooms.id"))
+    characters=relationship("Character", secondary=room, backref="the_characters")
 
     def __repr__(self):
         return f"Enemy {self.id}: " \
@@ -34,19 +75,22 @@ class Enemy (Base):
             + f"type {self.type}"\
             + f"HP {self.health_point}"\
             + f"AP {self.attack_point}"\
-            + f"room number {self.room_id}"
+            
 
-class Room (Base): 
-    __tablename__='rooms'
-    id = Column (Integer(), primary_key=True)
-    name = Column(String())
-    room_type= Column(String())
+        
+# class Room (Base): 
+#     __tablename__='rooms'
+#     id = Column (Integer(), primary_key=True)
+#     name = Column(String())
+#     room_type= Column(String())
+#     char_id =Column(Integer(), ForeignKey("enemies.id"))
+#     enemy_id=Column(Integer(), ForeignKey ("characters.id"))
   
 
-    def __repr__(self):
-        return f"Room {self.id}: " \
-            + f"{self.name}, " \
-            + f"type {self.room_typetype}"
-            
-   
+#     def __repr__(self):
+#         return f"Room {self.id}: " \
+#             + f"{self.name}, " \
+#             + f"type {self.room_type}"\
+#             + f"Character Id {self.char_id}"\
+#             + f"Enemy Id {self.enemy_id}"
 
--- lib/db/seed.py
+++ lib/db/seed.py
@@ -1,11 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from models import Character, Enemy
-import ipdb; ipdb.set_trace()
+from models import Character
+# import ipdb; ipdb.set_trace()
 
-from faker import Faker
-fake = Faker()
+# from faker import Faker
+# fake = Faker()
 
 if __name__ == '__main__':
     engine = create_engine('sqlite:///mini_survival_game.db')
@@ -14,10 +14,27 @@ if __name__ == '__main__':
 
     session.query(Character).delete()
 
+characters= [
+    Character (name="James Park", class_job= "knight",health_point= "70", 
+               combat_word= "Fight", attack_point = "25"),
+    Character (name="Karen Smith", class_job= "priest", health_point= "40",
+               combat_word= "Holy Fire", attack_point = "10"),
+    Character (name="Harry Dime", class_job= "mage",
+               health_point= "50", combat_word= "Ice Storm", attack_point = "20"),
+    Character (name="Nick Super", class_job= "archer",
+               health_point= "60", combat_word= "Quick Shot",attack_point = "15"),
+
+]
+
+session.bulk_save_objects (characters)
+session.commit ()
+
+characters =session.query (Character).all()
+
 
 #create a random enemies name
-for _ in range (5):
-    name=fake.name()
-    enemy = Enemy (name=fake.name())
-    session.add(enemy)
-    session.commit()
+# for _ in range (5):
+#     name=fake.name()
+#     enemy = Enemy (name=fake.name())
+#     session.add(enemy)
+#     session.commit()
